900a3b93ff5c5d595b996e92c755b79014442ab4
Diogo Munaro Vieira <diogo.munaro@corp.globo.com>
2017-04-13 21:04:38 -0300

    First commit

---
+++ src/main/scala/AkkaHttpMicroservice.scala
@@ -0,0 +1,61 @@
+import akka.actor.ActorSystem
+import akka.event.{LoggingAdapter, Logging}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import java.io.IOException
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.math._
+import spray.json.DefaultJsonProtocol
+
+case class Hello(hello: String)
+case class SqlQuery(query: String)
+
+trait Protocols extends DefaultJsonProtocol {
+  implicit val helloFormat = jsonFormat1(Hello.apply)
+  implicit val sqlQueryFormat = jsonFormat1(SqlQuery.apply)
+}
+
+trait Service extends Protocols {
+  implicit val system: ActorSystem
+  implicit def executor: ExecutionContextExecutor
+  implicit val materializer: Materializer
+
+  def config: Config
+  val logger: LoggingAdapter
+
+  val routes = {
+    logRequestResult("akka-http-microservice") {
+      pathPrefix("hello") {
+        (get & path(Segment)) { hello =>
+          complete {
+            Hello(hello)
+          }
+        } ~
+        (post & entity(as[SqlQuery])) { sqlQuery =>
+          complete(sqlQuery)
+        }
+      }
+    }
+  }
+}
+
+object AkkaHttpMicroservice extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorMaterializer()
+
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+}
